915c21c31ab18b113cb6b7aa654452cf84e63ba3
Kunalkaklotar <kunalkaklotar@gmail.com>
2023-04-07 19:10:20 +0530

    counter-title

--- src/App.js
+++ src/App.js
@@ -2,40 +2,23 @@ import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
-  let [count, setCount] = useState(0);
+  let [counter, setCounter] = useState(0);
   const setValue = () => {
-    setCount(++count);
-    document.title = "countValue" + " = " + " " + `${count}`;
+    setCounter(++counter);
+    document.title = "countValue" + " = " + " " + `${counter}`;
   };
 
   return (
-    <div
-      style={{
-        display: "flex",
-        flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "center",
-        height: "100vh",
-      }}
->
-      <button
-        style={{
-          color: "white",
-          backgroundColor: "black",
-          border: "none",
-          padding: "12px 24px",
-          borderRadius: "20px",
-          cursor: "pointer",
-          fontSize: "18px",
-          fontWeight: "500",
-          letterSpacing: "1px",
-          marginBottom: "20px",
-        }}
-        onClick={() => setValue()}
-      >
-        click
-      </button>
-      <h1>{count}</h1>
+    <div style={{ display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center", height: "100vh", }} >
+
+
+    <button
+      style={{ color: "black", backgroundColor: "white", fontSize: "24px", borderRadius: "10px", padding: "12px 24px", }}
+    onClick={() => setValue()} > click </button>
+
+    <h1 style={{fontSize: "80px", color: "red",}}>{counter}</h1>
+
+
     </div>
   );
 }
