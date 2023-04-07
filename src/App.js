9c8724412faf26b4ec9f20d52a159a68b738e355
Kunalkaklotar <kunalkaklotar@gmail.com>
2023-04-07 18:46:15 +0530

    counter-title

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
+import "./App.css";
 
 function App() {
+  let [count, setCount] = useState(0);
+  const setValue = () => {
+    setCount(++count);
+    document.title = "countValue" + " = " + " " + `${count}`;
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}
+>
+      <button
+        style={{
+          color: "white",
+          backgroundColor: "black",
+          border: "none",
+          padding: "12px 24px",
+          borderRadius: "20px",
+          cursor: "pointer",
+          fontSize: "18px",
+          fontWeight: "500",
+          letterSpacing: "1px",
+          marginBottom: "20px",
+        }}
+        onClick={() => setValue()}
+      >
+        click
+      </button>
+      <h1>{count}</h1>
     </div>
   );
 }
